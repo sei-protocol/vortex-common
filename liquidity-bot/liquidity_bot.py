0215b66da9b4dbe5cda583e1a000fe81115581e0
Cyson <cyson@Cysons-MBP.attlocal.net>
2022-10-13 16:00:42 -0700

    Update bot

--- liquidity-bot/liquidity_bot.py
+++ liquidity-bot/liquidity_bot.py
@@ -1,9 +1,11 @@
 import argparse
 import json
+from os import truncate
 import random
 import subprocess
 import sys
 import time
+import random
 from pathlib import Path
 
 base_price = 0
@@ -20,6 +22,9 @@ EXCHANGE_RATES_TMPL = (
     " query oracle exchange-rates | grep -A 2 \"{denom}\" | "
     "sed -n 's/.*exchange_rate: \"//p' | sed -n 's/\"//p'"
  )
+SEND_FUND_TMPL = (
+    " seid tx bank send -y admin {account} 1000000000000factory/sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc/uust2"
+)
 
 class OrderData:
     def __init__(self, position_effect="Open", leverage="1") -> None:
@@ -33,29 +38,6 @@ class LiquidityBot:
         self.contract = contract
         self.binary = binary
         self.chain_id = chain_id
-
-    def generate_random_order(account_address):
-        ot = "Long"
-        scale = ((3*(random.uniform(0, 1)**3)+1)**2)/50
-        p = round(base_price * (1 + scale), 2)
-        l = 1
-        q = random.randint(quantity_floor, quantity_ceiling)
-
-        if random.getrandbits(1):
-            ot = "Short"
-            p = round(base_price * (1 - scale), 2)
-            l += 1
-
-        return PLACE_ORDER.format(contract=contract_address,
-                                account=account_address,
-                                order_type=ot,
-                                price=p,
-                                quantity=q,
-                                price_denom="SEI",
-                                asset_denom="ATOM",
-                                leverage=l,
-                                amount=round(p * q * 1000500)
-                                )
 
     def get_oracle_price(self):
         result = subprocess.check_output(
@@ -77,12 +59,12 @@ class LiquidityBot:
         data_json = json.dumps(OrderData().__dict__)
 
         # randomize order price and quantity
-        quantity = round(random.uniform(0, 1) * 10, 2)
+        quantity = truncate(random.uniform(0, 1) * 10, 1)
         print("quantity", quantity)
         if order_type == "SHORT":
-            price = self.get_oracle_price() + round(random.uniform(0, 1) * 5, 2)
+            price = self.get_oracle_price() + truncate(random.uniform(0, 1) * 5, 1)
         else:
-            price = self.get_oracle_price() - round(random.uniform(0, 1) * 5, 2)
+            price = self.get_oracle_price() - truncate(random.uniform(0, 1) * 5, 1)
 
         result = subprocess.check_output(
             [
@@ -104,6 +86,19 @@ class LiquidityBot:
         )
         print(result)
 
+    def send_fund(self):
+        print("send fund")
+        result = subprocess.check_output(
+            [
+                CMD.format(password=self.password, binary=self.binary) + 
+                SEND_FUND_TMPL.format(
+                    key=self.key, 
+                ) 
+            ],
+            stderr=subprocess.STDOUT,
+            shell=True,
+        )
+        print(result)
 
 if __name__ == "__main__":
     parser=argparse.ArgumentParser()
@@ -115,8 +110,18 @@ if __name__ == "__main__":
     args=parser.parse_args()
 
     lb = LiquidityBot(args.key, args.password, args.contract, args.chain_id, args.binary)
+
+    accounts = open(str(Path.home()) + 'accounts.txt', 'r')
     
     while True:
-        lb.place_order("LONG")
-        lb.place_order("SHORT")
+        account = accounts.readline()
+        if not account:
+            break
+
+        lb.key = account
+        side = random.random()
+        if side < 0.33 or side > 0.66:
+            lb.place_order("LONG")
+        if side > 0.33:
+            lb.place_order("SHORT")
         time.sleep(30)
